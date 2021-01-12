a83f7454157401a38efd58de8487b6c05423f29f
EugenyTR <28475&~7403klKJHF>
2021-01-12 17:59:01 +0600

    getInitialProps

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -1,12 +1,25 @@
 import {useRouter} from "next/router";
 import {MainLayout} from "../../components/MainLayout";
+import Link from 'next/link';
 
-export default function Post() {
-    const router = useRouter()
-    console.log(router);
+export default function Post({ post }) {
+    // const router = useRouter()
     return (
         <MainLayout>
-            <h1>Post {router.query.postId}</h1>
+            <h1>{post.title}</h1>
+            <hr />
+            <p>{post.body}</p>
+            <Link href={"/posts"}><a>Back to all posts</a></Link>
         </MainLayout>
     )
 }
+
+
+Post.getInitialProps = async (ctx) => {
+    const response = await fetch(`http://localhost:4200/posts/${ctx.query.id}`);
+    const post = await response.json();
+
+    return {
+        post
+    }
+};
